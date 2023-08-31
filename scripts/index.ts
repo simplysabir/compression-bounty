920c1751f0d31efe8b6880d57e1b0a339371f69f
simplysabir <simplysabir@gmail.com>
2023-08-31 18:37:10 +0530

    exported

---
+++ scripts/index.ts
@@ -0,0 +1,9 @@
+import { createCollection } from './createCollection';
+import { createTree } from './createTree';
+import { mintCompressedNFT } from './mintCompressedNFT';
+
+export {
+    createCollection,
+    createTree,
+    mintCompressedNFT
+}
